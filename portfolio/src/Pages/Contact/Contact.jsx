cfba0a437e1ab4a7fac46b7b64a2c4548f6ed113
jaiminbvmengg <jaiminbvmengg@gmail.com>
2025-12-04 21:59:09 +0530

    Update Contact.jsx

--- portfolio/src/Pages/Contact/Contact.jsx
+++ portfolio/src/Pages/Contact/Contact.jsx
@@ -4,8 +4,8 @@ const Contact = () => {
   return (
     <section id="contact">
       <h2>Contact Me</h2>
-      <p>Email: johndoe@example.com</p>
-      <p>LinkedIn: <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noreferrer">linkedin.com/in/yourprofile</a></p>
+      <p>Email: jaiminbvmengg@gmail.com</p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/jaimin-parmar-758658284?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank" rel="noreferrer">linkedin.com/in/yourprofile</a></p>
       <p>GitHub: <a href="https://github.com/yourusername" target="_blank" rel="noreferrer">github.com/yourusername</a></p>
     </section>
   );
