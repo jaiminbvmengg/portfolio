d5313d32612d203819fcf3d0f4fe1594377b20af
jaiminbvmengg <jaiminbvmengg@gmail.com>
2025-12-04 22:00:10 +0530

    Update Contact.jsx

--- portfolio/src/Pages/Contact/Contact.jsx
+++ portfolio/src/Pages/Contact/Contact.jsx
@@ -5,8 +5,8 @@ const Contact = () => {
     <section id="contact">
       <h2>Contact Me</h2>
       <p>Email: jaiminbvmengg@gmail.com</p>
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/jaimin-parmar-758658284?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank" rel="noreferrer">linkedin.com/in/yourprofile</a></p>
-      <p>GitHub: <a href="https://github.com/jaiminbvmengg" target="_blank" rel="noreferrer">github.com/yourusername</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/jaimin-parmar-758658284?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank" rel="noreferrer">Linkedin</a></p>
+      <p>GitHub: <a href="https://github.com/jaiminbvmengg" target="_blank" rel="noreferrer">Github</a></p>
     </section>
   );
 };
